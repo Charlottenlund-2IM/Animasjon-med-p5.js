d8d1187533f4c03b2c11f8e36d93b5b1c20ba757
kadalsaune <kadalsaune@gmail.com>
2021-10-08 10:45:09 +0200

    Add files via upload

---
+++ animate.js
@@ -0,0 +1,15 @@
+//Globale variabler
+
+
+//Setup-funksjon: kjører en gang når programmet starter
+function setup() {
+    let myCanvas = createCanvas(400, 400);
+    myCanvas.parent("p5canvas");
+    
+  }
+
+  //draw-funksjon: kjører i løkke så lenge programmet kjører.
+  //Bruker til å få objekter til å bevege seg.
+function draw(){
+
+  }
